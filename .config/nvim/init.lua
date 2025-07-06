259d7e473e73818e643d7e6473b62616759226eb
Wei Lim <weikanglim@gmail.com>
2025-07-05 18:08:15 -0700

    nvim: ff with hidden

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -205,7 +205,13 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>ff', function()
+        builtin.find_files {
+          find_command = function()
+            return { 'rg', '--files', '--color', 'never', '--hidden', '--glob=!.git' }
+          end,
+        }
+      end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
