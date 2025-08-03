9fc2890199f9ad07b38c557f0313b082ac3917e3
Wei Lim <weikanglim@gmail.com>
2025-08-03 14:13:50 -0700

    enable folding

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -70,6 +70,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Fold expressions
+vim.opt.foldenable = false
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
